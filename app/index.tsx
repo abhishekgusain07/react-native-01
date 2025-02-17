0b2a41e9a1978a19bafb39701ce242e6014342ce
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-02-17 12:28:21 +0530

    navigating

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
-import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native'
+import { View, Text, Button, StyleSheet, ImageBackground, Pressable } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 const icedCoffee = require('@/assets/images/iced-coffee.png')
 
 const app = () => {
@@ -11,6 +12,11 @@ const app = () => {
         resizeMode='cover'
         >
         <Text style={styles.text}>Iced out Cafe</Text>
+        <Link href="/explore" style={{ marginHorizontal: 'auto'}} asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>address</Text>
+        </Pressable>
+        </Link>
       </ImageBackground>
     </View>
   )
@@ -33,7 +39,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0.5)',
-  }
+  },
+  link:{
+    marginTop: 15,
+    paddingVertical: 15,
+    color: 'white',
+    fontWeight: "bold",
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 4,
+    textDecorationLine: 'underline',
+  },
+  button:{
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    marginTop:50,
+    height: 60,
+    borderRadius: 20,
+    padding: 6,
+    justifyContent: 'center',
+  },
+  buttonText:{
+    fontSize: 16,
+    color: 'white',
+    fontWeight: "bold",
+    textAlign: 'center',
+    padding: 4,
+  },
 })
 
 export default app
